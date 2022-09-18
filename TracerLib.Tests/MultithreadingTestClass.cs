1cad7054e7c56f80f6749e1d1c8a7ce23bce04cd
Yuri Voitovich <voitovich200233@gmail.com>
2022-09-18 17:47:51 +0300

    MultithreadingTracerTest created

---
+++ TracerLib.Tests/MultithreadingTestClass.cs
@@ -0,0 +1,27 @@
+﻿using SPP_tracer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TracerLib.Tests
+{
+    public class MultithreadingTestClass
+    {   
+        ITracer tracer;
+
+        public MultithreadingTestClass(ITracer tracer)
+        {
+            this.tracer = tracer;
+        }
+
+        public void start()
+        {
+            InnerMethodsTestClass testClass = new InnerMethodsTestClass(tracer);
+            testClass.TestMethod();
+            MultiThreadingTracerTest.AddTraceResult(tracer.GetTraceResult());
+        }
+
+    }
+}
---
+++ TracerLib.Tests/MultithreadingTracerTest.cs
@@ -0,0 +1,54 @@
+﻿using SPP_tracer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TracerLib.Tests
+{
+    [TestClass]
+    public class MultiThreadingTracerTest
+    {
+        public static readonly object balanceLock = new object();
+        public static List<TraceResult> traceResults = new List<TraceResult>();
+
+        public static void AddTraceResult(TraceResult traceResult)
+        {
+            lock (balanceLock)
+            {
+                traceResults.Add(traceResult);
+            }
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            List<Thread> threads = new List<Thread>();
+            for (int i = 0; i < 3; i++)
+            {
+                Thread myThread = new Thread(foo);
+                threads.Add(myThread);
+                myThread.Start();
+            }
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+        }
+
+        private static void foo()
+        {
+            Tracer tracer = new Tracer();
+            MultithreadingTestClass A = new MultithreadingTestClass(tracer);
+            A.start();
+        }
+
+        [TestMethod]
+        public void TestTraceResultsArraySize()
+        {
+            Assert.AreEqual(3, traceResults.Count);
+        }
+
+    }
+}
