10ecc6e2cf1fdde9fb3971291d34d8458f4c651f
Yuri Voitovich <voitovich200233@gmail.com>
2022-09-18 17:47:37 +0300

    InnerMethodsTracerTest created

---
+++ TracerLib.Tests/InnerMethodsTestClass.cs
@@ -0,0 +1,28 @@
+﻿using SPP_tracer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TracerLib.Tests
+{
+    internal class InnerMethodsTestClass
+    {
+        ITracer tracer;
+        OneMethodTestClass testClass;
+        public InnerMethodsTestClass(ITracer tracer)
+        {
+            this.tracer = tracer;
+            testClass = new OneMethodTestClass(tracer);
+        }
+
+        public void TestMethod()
+        {
+            tracer.StartTrace();
+            testClass.TestMethod();
+            testClass.TestMethod();
+            tracer.StopTrace();
+        }
+    }
+}
---
+++ TracerLib.Tests/InnerMethodsTracerTest.cs
@@ -0,0 +1,33 @@
+﻿using SPP_tracer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TracerLib.Tests
+{
+    [TestClass]
+    public class InnerMethodsTracerTest
+    {
+
+        public static TraceResult traceResult;
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            Tracer tracer = new Tracer();
+            InnerMethodsTestClass testClass = new InnerMethodsTestClass(tracer);
+            testClass.TestMethod();
+            testClass.TestMethod();
+            traceResult = tracer.GetTraceResult();
+        }
+
+        [TestMethod]
+        public void TestTracedMethodsArrayCount()
+        {
+            Assert.AreEqual(2, traceResult.TracedMethods[0].TracedMethods.Count());
+        }
+
+        [TestMethod]
+        public void TestThreadTime()
+        {
+            Assert.AreEqual(traceResult.TracedMethods[0].Time + traceResult.TracedMethods[1].Time, traceResult.ThreadTime);
+        }
+    }
+}
